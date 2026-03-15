faeeea583fcef461a50438a1adaefb3bc23cee69
Eloy <157186883+Eloysday@users.noreply.github.com>
2026-03-15 17:31:37 +0800

    Update note1.mk

--- mynote/python issue/note1.mk
+++ mynote/python issue/note1.mk
@@ -1,5 +1,9 @@
-#--only-binary=:all: 参数会让 pip 优先下载预编译包，能避免绝大多数 Windows 下的编译报错，是快速解决这类问题的通用技巧。
-# 强制安装预编译版 greenlet
-pip install --only-binary=:all: greenlet
-# 之后再安装你需要的库（如 fastapi 等）
+# 环境配置
+1. 换源必做：pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+2. 安装依赖：pip install pymysql pandas plotly streamlit streamlit-autorefresh fastapi uvicorn python-dotenv
+3. 运行看板：streamlit run xxx.py
+4. 运行接口：uvicorn xxx:app --reload
+
+uvicorn：FastAPI 专用启动工具，负责运行数据接口服务
+#--only-binary=:all: 参数会让 pip 优先下载预编译包，能避免绝大多数 Windows 下的编译报错，之后再安装需要的库（如 fastapi 等）
 pip install fastapi uvicorn sqlalchemy pymysql --only-binary=:all:
